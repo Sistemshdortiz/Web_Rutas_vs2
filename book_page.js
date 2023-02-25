0ad767039b00ecc941e6ca5b6a37f467941f6774
Sistemshdortiz <90852224+Sistemshdortiz@users.noreply.github.com>
2023-02-25 18:41:12 +0100

    libro con adeventlisener

---
+++ book_page.js
@@ -0,0 +1,85 @@
+// References to DOM Elements
+const book = document.querySelector("#book");
+
+const paper1 = document.querySelector("#p1");
+const paper2 = document.querySelector("#p2");
+const paper3 = document.querySelector("#p3");
+
+
+//Asignación eventos a cada pagina de mi libro
+const pagesFront = document.querySelectorAll('.front');
+pagesFront.forEach(page => {
+    page.addEventListener('click', goNextPage);
+});
+
+const pagesBack = document.querySelectorAll('.back');
+pagesBack.forEach(pagBack => {
+    pagBack.addEventListener('click', goPrevPage);
+});
+
+// Lógica
+let currentLocation = 1;
+let numOfPapers = 3;
+let maxLocation = numOfPapers + 1;
+
+function openBook() {
+    book.style.transform = "translateX(50%)";
+}
+
+function closeBook(isAtBeginning) {
+    if (isAtBeginning) {
+        book.style.transform = "translateX(0%)";
+
+    } else {
+        book.style.transform = "translateX(100%)";
+    }
+}
+
+function goNextPage() {
+    if (currentLocation < maxLocation) {
+        switch (currentLocation) {
+            case 1:
+                openBook();
+                paper1.classList.add("flipped");
+                paper1.style.zIndex = 1;
+                break;
+            case 2:
+                paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 3:
+                paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
+                closeBook(false);
+                break;
+            default:
+                throw new Error("unkown state");
+        }
+        currentLocation++;
+    }
+}
+
+function goPrevPage() {
+    if (currentLocation > 1) {
+        switch (currentLocation) {
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3;
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 4:
+                openBook();
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 1;
+                break;
+            default:
+                throw new Error("unkown state");
+        }
+
+        currentLocation--;
+    }
+}
